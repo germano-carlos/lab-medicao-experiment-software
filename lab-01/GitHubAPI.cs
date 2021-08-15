d6aa5ac4c0e0d5a59e317bc6299d1090272a14e9
Carlos Germano <carlos.germano@cmtecnologia.com.br>
2021-08-15 12:13:12 -0300

    Initial Commit

---
+++ lab-01/GitHubAPI.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_01
+{
+    class GitHubAPI
+    {
+        protected static string _url = "https://api.github.com/graphql";
+        protected static string _token = "bearer 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";
+
+        public static T Request<T>(string query)
+        {
+            JObject j = new JObject();
+            j["query"] = query;
+
+            var client = new RestClient(_url);
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Authorization", _token);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddParameter("application/json", j.ToString(), ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+
+            return JsonConvert.DeserializeObject<T>(response.Content);
+        }
+    }
+}
---
+++ lab-01/Program.cs
@@ -0,0 +1,44 @@
+﻿using GraphQL;
+using GraphQL.SystemTextJson;
+using GraphQL.Types;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Threading.Tasks;
+
+namespace lab_01
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Run();
+            Console.ReadKey();
+        }
+
+        public static void Run()
+        {
+            var list = BuscaRepositorios();
+            Console.WriteLine(list);
+        }
+
+        public static JObject BuscaRepositorios(int quantidade = 100)
+        {
+            string query = @"{
+                search (query: ""stars:>10000"", type:REPOSITORY, first:" + quantidade + @") {
+                    nodes {
+                        ... on Repository {
+                            nameWithOwner
+                            createdAt
+                            pushedAt
+                            stargazers {
+                                totalCount
+                            }
+                        }
+                    }
+                }
+            }";
+            return GitHubAPI.Request<JObject>(query);
+        }
+    }
+}
